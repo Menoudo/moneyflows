bf1497242bdea8ae69f4176ce76006bce95aec3a
Menoudo <priahin@list.ru>
2018-08-05 01:59:19 +0500

    updates

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 moneyflows webhook info from **IFTTT** and show actual in **Telegram** message
 
+# Run server
+__export APP_SETTINGS=config.DevelopmentConfig__
+__python3 app.py__
--- config.py
+++ config.py
@@ -6,8 +6,32 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class AppConfig(object):
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed1'
+    # Statement for enabling the development environment
+    DEVELOPMENT = True
+    DEBUG = True
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+
+    # Define the database - we are working with
+    # SQLite for this example
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    DATABASE_CONNECT_OPTIONS = {}
+
+    # Application threads. A common general assumption is
+    # using 2 per available processor cores - to handle
+    # incoming requests using one and performing background
+    # operations using the other.
+    THREADS_PER_PAGE = 2
+
+    # Enable protection agains *Cross-site Request Forgery (CSRF)*
+    CSRF_ENABLED     = True
+
+    # Use a secure, unique and absolutely secret key for
+    # signing the data. 
+    CSRF_SESSION_KEY = "secret1"
+
+    # Secret key for signing cookies
+    SECRET_KEY = "secret1"
 
 
 class ProductionConfig(AppConfig):
@@ -26,8 +50,3 @@ class DevelopmentConfig(AppConfig):
 
 class TestingConfig(AppConfig):
     TESTING = True
-
-
-class BotConfig(object):
-    TELEGRAM_TOKEN = ''
-    ACESS_KEY = ''

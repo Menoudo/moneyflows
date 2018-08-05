58bff26efd4b541dec374fad0c44bb7056c71f3e
Menoudo <33178064+menoudo@users.noreply.github.com>
2018-08-05 12:17:42 +0500

    Optimize requirements fix config to disable sqlalchemy warning

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ class AppConfig(object):
     TESTING = False
     # Statement for enabling the development environment
     DEVELOPMENT = True
-    DEBUG = True
 
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
     # Define the database - we are working with
     # SQLite for this example
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DATABASE_CONNECT_OPTIONS = {}
 
     # Application threads. A common general assumption is
@@ -24,7 +24,7 @@ class AppConfig(object):
     THREADS_PER_PAGE = 2
 
     # Enable protection agains *Cross-site Request Forgery (CSRF)*
-    CSRF_ENABLED     = True
+    CSRF_ENABLED = True
 
     # Use a secure, unique and absolutely secret key for
     # signing the data. 
